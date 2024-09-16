e1d23d3a6e40a05ac9ad7b07e72f41f07840d636
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2024-09-16 18:25:51 +0000

    did something

---
+++ wordle.py
@@ -0,0 +1,41 @@
+word = "three"
+
+
+default = '\033[0m'
+green = '\033[92m'
+yellow = '\033[33m'
+
+
+
+def generate_hint(user_guess):
+    color - default
+    hint = " "
+    for i in range(5):
+        if (user_guess[1] == word[i]):
+            color = green
+        elif (user_guess[1] in word):
+            color = yellow
+        else:
+            color = default 
+        
+        hint = hint + color + user_guess[i] + default
+
+        return hint
+    
+    return user_guess 
+
+
+
+def game_loop():
+    # looping 6 times
+    guess = ""
+    for i in range(6):
+        guess = input("what is your guess: ")
+        if (guess == word):
+            print("congratulations!!!")
+            break
+
+game_loop()
+
+
+
