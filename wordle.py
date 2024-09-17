5192e59699983e38fd287089b1fe672c16ff8b87
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2024-09-17 17:26:46 +0000

    done

--- wordle.py
+++ wordle.py
@@ -1,4 +1,9 @@
-word = "three"
+import random
+
+possible_words = ["great", "thumb", "seven", "three", "kaleb", "salad"] 
+
+
+word = random.choice(possible_words)
 
 
 default = '\033[0m'
@@ -28,7 +33,7 @@ def generate_hint(user_guess):
 
 def game_loop():
     # looping 6 times
-    guess = ""
+    guess = " "
     for i in range(6):
         guess = input("what is your guess: ")
         if (guess == word):
